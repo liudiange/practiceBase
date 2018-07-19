3839b9f716c68a43f8f2b3c7fff833cc512769d2
shaoyeliudiange <shaoyeliudiange@163.com>
2018-07-19 16:29:08 +0800

    block的部分修改

--- block的本质问题/block的本质问题/main.m
+++ block的本质问题/block的本质问题/main.m
@@ -53,47 +53,58 @@
 //typedef void(^MyBlock)(void);
 
 
-struct __Block_byref_age_0 {
-    void *__isa;
-    struct __Block_byref_age_0 *__forwarding;
-    int __flags;
-    int __size;
-    int age;
-};
-struct __block_impl {
-    void *isa;
-    int Flags;
-    int Reserved;
-    void *FuncPtr;
-};
-struct __main_block_desc_0 {
-    size_t reserved;
-    size_t Block_size;
-    void (*copy)(void);
-    void (*dispose)(void);
-};
-struct __main_block_impl_0 {
-    struct __block_impl impl;
-    struct __main_block_desc_0* Desc;
-    struct __Block_byref_age_0 *age; // by ref
-};
+//struct __Block_byref_age_0 {
+//    void *__isa;
+//    struct __Block_byref_age_0 *__forwarding;
+//    int __flags;
+//    int __size;
+//    int age;
+//};
+//struct __block_impl {
+//    void *isa;
+//    int Flags;
+//    int Reserved;
+//    void *FuncPtr;
+//};
+//struct __main_block_desc_0 {
+//    size_t reserved;
+//    size_t Block_size;
+//    void (*copy)(void);
+//    void (*dispose)(void);
+//};
+//struct __main_block_impl_0 {
+//    struct __block_impl impl;
+//    struct __main_block_desc_0* Desc;
+//    struct __Block_byref_age_0 *age; // by ref
+//};
 
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        __block int age = 10;
+        DGPerson *person = [[DGPerson alloc] init];
+        __block __weak DGPerson *weakPerson = person;
         void (^myBlock)(void) = ^(){
-            age = 20;
-            NSLog(@"age = %d",age);
+            person.age = 20;
+            NSLog(@"age = %d",weakPerson.age);
         };
         myBlock();
         
-        // 将block 替换成我们写的那个结构体
-        struct __main_block_impl_0 *block = (__bridge struct __main_block_impl_0 *)myBlock;
         
-        NSLog(@"age 的 address = %p",&age);
+        
+        
+//        __block int age = 10;
+//        void (^myBlock)(void) = ^(){
+//            age = 20;
+//            NSLog(@"age = %d",age);
+//        };
+//        myBlock();
+//
+//        // 将block 替换成我们写的那个结构体
+//        struct __main_block_impl_0 *block = (__bridge struct __main_block_impl_0 *)myBlock;
+//
+//        NSLog(@"age 的 address = %p",&age);
         
         
 //        MyBlock myBlock;
